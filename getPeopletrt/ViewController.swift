cfe0823c7b71172f8b05fe1323e07b44ca9a5844
trt14 <tarooti14dev@gmail.com>
2021-12-28 14:48:00 +0300

    Initial Commit

---
+++ getPeopletrt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  getPeopletrt
+//
+//  Created by Tarooti on 24/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
